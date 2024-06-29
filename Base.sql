0e1d5e9507edd8d1bbe3c9650bbf24c7963ec924
Aldonis12 <micklewis.aldonis@gmail.com>
2024-06-29 20:47:27 +0300

    update

--- Base.sql
+++ Base.sql
@@ -28,5 +28,13 @@ create table client(
     id serial primary key not null,
     email varchar(30) UNIQUE ,
     id_type_perso integer references typePersonne (id)
+);
 
+create table bien(
+    id serial primary key not null,
+    nom varchar(50),
+    description varchar(150),
+    region varchar(50),
+    loyer integer,
+    id_proprio integer references proprio (id)
 );
